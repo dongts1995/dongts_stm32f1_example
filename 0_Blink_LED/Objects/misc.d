e87306932c6ff81fa499febb8dce971e0f8e89a6
Sỹ Đồng Trịnh <tsdtowafus@gmail.com>
2021-08-16 23:11:56 +0700

    sua path oke

--- 0_Blink_LED/Objects/misc.d
+++ 0_Blink_LED/Objects/misc.d
@@ -4,7 +4,7 @@
 .\objects\misc.o: ..\..\STM32F10x_StdPeriph_Lib_V3.5.0\Libraries\CMSIS\CM3\CoreSupport\core_cm3.h
 .\objects\misc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\misc.o: ..\..\STM32F10x_StdPeriph_Lib_V3.5.0\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
-.\objects\misc.o: .\src\stm32f10x_conf.h
+.\objects\misc.o: ..\..\B1_Blink_LED\src\stm32f10x_conf.h
 .\objects\misc.o: ..\..\STM32F10x_StdPeriph_Lib_V3.5.0\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_adc.h
 .\objects\misc.o: ..\..\STM32F10x_StdPeriph_Lib_V3.5.0\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
 .\objects\misc.o: ..\..\STM32F10x_StdPeriph_Lib_V3.5.0\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_bkp.h
